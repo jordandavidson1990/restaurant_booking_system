bc6584fea1e6b05f157cc8be9305257b5f3c6e9c
Jordan Davidson <jordandavidson1990@hotmail.com>
2018-12-07 19:32:25 +0000

    start of customer models

--- backend/src/main/java/com/codeclan/restaurantService/models/Customer.java
+++ backend/src/main/java/com/codeclan/restaurantService/models/Customer.java
@@ -1,17 +1,91 @@
 package com.codeclan.restaurantService.models;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
-@Table(name="customers")
-class Customer implements Serializable {
+@Table(name = "customers")
+public class Customer implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     @Column
     private String firstName;
+
     @Column
     private String lastName;
+
     @Column
-    private
+    private String email;
+
+    @Column(name="contact_number")
+    private int contactNumber;
+
+    @OneToMany(mappedBy = "customer")
+    private List<Booking>bookings;
+
+    public Customer(String firstName, String lastName, String email, int contactNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.contactNumber = contactNumber;
+        this.bookings = new ArrayList<>();
+    }
+
+    public Customer() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(int contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public List<Booking> getBookings() {
+        return bookings;
+    }
+
+    public void setBookings(List<Booking> bookings) {
+        this.bookings = bookings;
+    }
 }
--- backend/src/test/java/com/codeclan/restaurantService/RestaurantServiceApplicationTests.java
+++ backend/src/test/java/com/codeclan/restaurantService/RestaurantServiceApplicationTests.java
@@ -13,4 +13,6 @@ public class RestaurantServiceApplicationTests {
 	public void contextLoads() {
 	}
 
+
+
 }
