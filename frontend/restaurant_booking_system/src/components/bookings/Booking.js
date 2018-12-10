9ead26d2049bd8141862bfab198fa78b88378a69
Craig Dunlop <craigdun9393@gmail.com>
2018-12-10 11:34:27 +0000

    changed booking form and uodated render function to show time

--- frontend/restaurant_booking_system/src/components/bookings/Booking.js
+++ frontend/restaurant_booking_system/src/components/bookings/Booking.js
@@ -2,9 +2,12 @@ import React from "react";
 
 const Booking = (props) => {
 
+
+const date =  props.booking.date
+const time = date.slice(11, 16)
   return(
     <div className = "single-booking">
-      <p> Time : {props.booking.time}</p>
+      <p> Time : {time}</p>
       <p>Customer: {props.booking.customer.firstName} {props.booking.customer.lastName}</p>
       <p>Number of Guests: {props.booking.numberOfCovers}</p>
       <p> Table: {props.booking.restTable.tableNumber}</p>
--- frontend/restaurant_booking_system/src/components/bookings/BookingForm.js
+++ frontend/restaurant_booking_system/src/components/bookings/BookingForm.js
@@ -6,23 +6,37 @@ const BookingForm = (props) => {
     event.preventDefault();
     const booking = {
       "date": event.target.date.value,
-      "firstName": event.target.firstName.value,
-      "lastName": event.target.lastName.value,
-      "email": event.target.email.value,
-      "contactNumber": event.target.contactNumber.value,
-      "numberOfCovers": event.target.numberOfCovers.value
+      "customer":event.target.customer.value,
+      "numberOfCovers": event.target.numberOfCovers.value,
+      "restTable": event.target.table.value
     }
     props.handleBookingPost(booking);
   }
 
+  const customerOptions = props.customers.map((customer) => {
+    return <option key={customer.id} value={customer._links.self.href}>{customer.firstName} {customer.lastName}</option>
+  })
+
+  const tableOptions = props.tables.map((table) => {
+    return(
+      <option key={table.id} value={table._links.self.href}>{table.tableNumber}</option>
+    )
+  })
+
   return(
     <form onSubmit = {handleSubmit}>
-      <input type="date" placeholder = "date" name = "date"/>
-      <input type="text" placeholder = "First Name" name = "firstName"/>
+      <input type="datetime-local" placeholder = "date" name = "date"/>
+      {/* <input type="text" placeholder = "First Name" name = "firstName"/>
       <input type="text" placeholder = "Last Name" name = "lastName"/>
       <input type="text" placeholder = "Email" name = "email"/>
-      <input type="number" placeholder = "Contact Number" name = "contactNumber"/>
+      <input type="number" placeholder = "Contact Number" name = "contactNumber"/> */}
+      <select name = "customer">
+        {customerOptions}
+      </select>
       <input type="number" placeholder = "No. of Guests" name = "numberOfCovers"/>
+      <select name = "table">
+        {tableOptions}
+      </select>
       <button type = "submit">Save Booking</button>
     </form>
   )
