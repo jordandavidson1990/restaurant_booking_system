6b222ca636a3427ac33ee594261708bd6dd613f0
Craig Dunlop <craigdun9393@gmail.com>
2018-12-10 14:34:56 +0000

    customerd are now sorted by their number of visits

--- frontend/restaurant_booking_system/src/components/customers/Customer.js
+++ frontend/restaurant_booking_system/src/components/customers/Customer.js
@@ -1,12 +1,16 @@
 import React from "react";
 
 const Customer = (props) => {
+const bookingArray = props.customer.bookings;
+const visits = bookingArray.length
+console.log(bookingArray);
 
+console.log(visits);
   return(
     <div className = "single-customer">
       <p> Name: {props.customer.firstName} {props.customer.lastName}: </p>
       <p> Contact number: {props.customer.contactNumber}, email:{props.customer.email}</p>
-      <p> Bookings: what do we need to see? </p>
+      <p> Number of Bookings: {visits}</p>
     </div>
   )
 }
