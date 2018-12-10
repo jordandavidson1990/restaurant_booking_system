0e227ce7f41b465288c4af54bdb858b6538961a6
Valentina Bonetti <valentina.bonetti@strath.ac.uk>
2018-12-10 11:20:17 +0000

    Add customer container, list and single customer and create route

--- frontend/restaurant_booking_system/src/App.js
+++ frontend/restaurant_booking_system/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import BookingContainer from "./container/bookings/bookingContainer";
 import NavBar from "./NavBar";
 import BookingFormContainer from "./container/bookings/BookingFormContainer";
+import CustomerContainer from "./container/customers/CustomerContainer";
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
           <React.Fragment>
             <NavBar/>
             <Switch>
+            <Route exact path="/customers" component = {CustomerContainer}/>
             <Route exact path="/bookings" component = {BookingContainer}/>
             <Route exact path="/bookings/new" component = {BookingFormContainer}/>
           </Switch>
