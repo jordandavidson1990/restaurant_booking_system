9fcf0e1c1b86c3a53c3140c13ac797ff536e78a6
Craig Dunlop <craigdun9393@gmail.com>
2018-12-11 09:56:57 +0000

    customers now shows total spend

--- frontend/restaurant_booking_system/src/components/customers/Customer.js
+++ frontend/restaurant_booking_system/src/components/customers/Customer.js
@@ -1,18 +1,38 @@
 import React from "react";
 
 const Customer = (props) => {
-const bookingArray = props.customer.bookings;
-const visits = bookingArray.length
-console.log(bookingArray);
+  const bookingArray = props.customer.bookings;
+  const visits = bookingArray.length
+  console.log(bookingArray);
+
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+
+
+
+  function totalSpend(){
+    let totalSpend;
+    if(bookingArray.length > 0){
+    totalSpend = bookingArray.map((booking) => {
+      return booking.receipt;
+    }).reduce(reducer);
+  } else{
+    totalSpend = 0;
+  }
+  return totalSpend;
+}
+
+console.log(totalSpend);
 
 console.log(visits);
-  return(
-    <div className = "single-customer">
-      <p> Name: {props.customer.firstName} {props.customer.lastName}: </p>
-      <p> Contact number: {props.customer.contactNumber}, email:{props.customer.email}</p>
-      <p> Number of Bookings: {visits}</p>
-    </div>
-  )
+return(
+  <div className = "single-customer">
+    <p> Name: {props.customer.firstName} {props.customer.lastName}: </p>
+    <p> Contact number: {props.customer.contactNumber}, email:{props.customer.email}</p>
+    <p> Number of Bookings: {visits}</p>
+    <p> Total Spent: £{totalSpend()}</p>
+  </div>
+)
 }
 
 export default Customer;
