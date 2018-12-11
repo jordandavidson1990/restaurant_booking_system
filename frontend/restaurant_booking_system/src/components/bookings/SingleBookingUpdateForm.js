7c7d5198aec00a70331b126855f2e69b12a01466
user <user@users-MacBook-Pro-107.local>
2018-12-11 15:56:35 +0000

    fixes date issue

--- frontend/restaurant_booking_system/src/components/bookings/SingleBookingUpdateForm.js
+++ frontend/restaurant_booking_system/src/components/bookings/SingleBookingUpdateForm.js
@@ -25,7 +25,6 @@ const SingleBookingUpdateForm = (props) => {
   const date = props.booking.date
   const dateSorted = date.slice(0,19)
   const formattedDate = moment(dateSorted).format("dddd Do MMM YYYY");
-
   const time = date.slice(11, 16)
   // const id = props.booking.id
 
@@ -43,14 +42,14 @@ const SingleBookingUpdateForm = (props) => {
     <div className="single-booking-update-container">
     <h6>First Name:</h6><input className="m-4 border rounded" type="text" placeholder="First Name" name="firstName" defaultValue={props.booking.customer.firstName}/>
     <h6>Last Name:</h6><input className="m-4 border rounded" type="text" placeholder="Last Name" name="lastName" defaultValue={props.booking.customer.lastName}/>
-    <h6>Existing Date:</h6><select className="m-4 border rounded" type="datetime-local" placeholder="date" name = "date" required>
-      {dateOptions}
-    </select>
+    // <h6>Existing Date:</h6><select className="m-4 border rounded" type="text" placeholder="nodate" name = "nodate" required>
+    //   {dateOptions}
+    // </select>
 
-    <h6>New Date:</h6><input className="m-4 border rounded" type="datetime-local" placeholder="datetime-local" placeholder="date" name = "date" defaultValue={formattedDate + time}/>
+    <h6>New Date:</h6><input className="m-4 border rounded" type="datetime-local" placeholder="datetime-local" placeholder="date" name = "date" defaultValue={props.booking.date}/>
 
     <h6>Number of Covers:</h6><input type="number" placeholder="numberOfCovers" name="numberOfCovers" defaultValue={props.booking.numberOfCovers}/>
-    <h6>Table Number:</h6><input type="number" placeholder="tableNumber" name="tableNumber" defaultValue={props.booking.tableNumber}/>
+    <h6>Table Number:</h6><input type="number" placeholder="tableNumber" name="tableNumber" defaultValue={props.booking.restTable.tableNumber}/>
     <h6>Receipts:</h6><input type = "number" placeholder = "receipt" name = "receipt" defaultValue = {props.booking.receipt}/>
     <button className="btn btn-info"type="submit">Save</button>
     </div>
