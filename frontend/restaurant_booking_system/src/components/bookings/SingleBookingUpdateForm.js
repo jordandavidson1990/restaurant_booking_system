7e5bb1652b376e86e62b521428da075599a35f19
user <user@users-MacBook-Pro-107.local>
2018-12-11 15:34:00 +0000

    works on date format - update not working yet

--- frontend/restaurant_booking_system/src/components/bookings/SingleBookingUpdateForm.js
+++ frontend/restaurant_booking_system/src/components/bookings/SingleBookingUpdateForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import moment from "moment";
 
 const SingleBookingUpdateForm = (props) => {
   if(!props.booking) return null
@@ -18,20 +19,39 @@ const SingleBookingUpdateForm = (props) => {
   }
 
   // const tableOptions = props.restTables.map((restTable) => {
-  //   return <option key={restTable.id} value={restTable._links.self.href}>{resttable}
+  //   return <option key={restTable.id} value={restTable._links.self.href}>{restTable}
   // })
+
+  const date = props.booking.date
+  const dateSorted = date.slice(0,19)
+  const formattedDate = moment(dateSorted).format("dddd Do MMM YYYY");
+
+  const time = date.slice(11, 16)
+  // const id = props.booking.id
+
+
+  const dateOptions = props.bookings.map((booking) => {
+    return (
+      <option key={booking.id} value={booking._links.self.href}>{formattedDate} {time}</option>
+    )
+  })
 
 
   return (
 
-    <form onSubmit={handleSubmit}>
+    <form className="container" onSubmit={handleSubmit}>
     <div className="single-booking-update-container">
-    <input type="text" placeholder="First Name" name="firstName" defaultValue={props.booking.customer.firstName}/>
-    <input type="text" placeholder="Last Name" name="lastName" defaultValue={props.booking.customer.lastName}/>
-    <input type="text" placeholder="Date" name="date" defaultValue={props.booking.date}/>
-    <input type="number" placeholder="numberOfCovers" name="numberOfCovers" defaultValue={props.booking.numberOfCovers}/>
-    <input type="number" placeholder="tableNumber" name="tableNumber" defaultValue={props.booking.tableNumber}/>
-    <input type = "number" placeholder = "receipt" name = "receipt" defaultValue = {props.booking.receipt}/>
+    <h6>First Name:</h6><input className="m-4 border rounded" type="text" placeholder="First Name" name="firstName" defaultValue={props.booking.customer.firstName}/>
+    <h6>Last Name:</h6><input className="m-4 border rounded" type="text" placeholder="Last Name" name="lastName" defaultValue={props.booking.customer.lastName}/>
+    <h6>Existing Date:</h6><select className="m-4 border rounded" type="datetime-local" placeholder="date" name = "date" required>
+      {dateOptions}
+    </select>
+
+    <h6>New Date:</h6><input className="m-4 border rounded" type="datetime-local" placeholder="datetime-local" placeholder="date" name = "date" defaultValue={formattedDate + time}/>
+
+    <h6>Number of Covers:</h6><input type="number" placeholder="numberOfCovers" name="numberOfCovers" defaultValue={props.booking.numberOfCovers}/>
+    <h6>Table Number:</h6><input type="number" placeholder="tableNumber" name="tableNumber" defaultValue={props.booking.tableNumber}/>
+    <h6>Receipts:</h6><input type = "number" placeholder = "receipt" name = "receipt" defaultValue = {props.booking.receipt}/>
     <button className="btn btn-info"type="submit">Save</button>
     </div>
     </form>
